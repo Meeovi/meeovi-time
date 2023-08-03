cf5601bfa2563923876d3aa9cdc9deb670e814fa
Sebastian hilton <bastianh86@outlook.com>
2023-08-03 16:38:06 -0700

    created website with working functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -7,38 +6,13 @@ import router from './router'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import { aliases, fa } from 'vuetify/iconsets/fa-svg'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 import './assets/styles.css'
-
-/* import the fontawesome core */
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-const app = createApp(App)
-
-app.use(createPinia())
-// This is important, we are going to let Nuxt worry about the CSS
-config.autoAddCss = false
-
-// You can add your icons directly in this plugin. See other examples for how you
-// can add other styles or just individual icons.
-library.add(fas)
 
 const vuetify = createVuetify({
   components,
   directives,
-  icons: {
-    defaultSet: 'fa',
-    aliases,
-    sets: {
-      fa,
-    },
-  },
 })
 
-createApp(App).use(router).use(vuetify).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).use(vuetify).mount('#app')
